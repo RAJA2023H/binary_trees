5edc8cedef5b9ea52d71f94010a9382ae270bcfa
Rajae <hemmanyrajae@gmail.com>
2024-03-26 01:02:10 -0700

    up

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,20 +10,17 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *child = malloc(sizeof(binary_tree_t));
-	binary_tree_t *temp = malloc(sizeof(binary_tree_t));
 
 	if ((child == NULL) || (parent == NULL))
 		return (NULL);
 	child = binary_tree_node(parent, value);
 	
-	temp = parent->left;
 	if (parent->left != NULL)
 	{
-		child->left = temp;
+		child->left = parent->left;
 		child->left->parent = child;
 	}
 	parent->left = child;
 	child->parent = parent;
-
 	return (child);
 }
