7293e30920c37578c09621b22621c75c17edff57
Rajae <hemmanyrajae@gmail.com>
2024-03-27 00:37:45 -0700

    deleting unecessery lines

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,24 +10,7 @@ void binary_tree_delete(binary_tree_t *tree)
 
 	if (tree != NULL)
 	{
-		Temp = malloc(sizeof(binary_tree_t));
-		if (Temp != NULL)
-		{
-			Temp = tree;
-			if (Temp->left != NULL)
-			{
-				Temp = Temp->left;
-				binary_tree_delete(Temp->left);
-				free(Temp->left);
-			}
-			if (Temp->right != NULL)
-			{
-				Temp = Temp->right;
-				binary_tree_delete(Temp->right);
-				free(Temp->right);
-			}
-		}
-		free(Temp);
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
 	}
-	free(tree);
-}
