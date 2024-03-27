d2775c362890a5b78c5a0283e1acc4da2004214a
Rajae <hemmanyrajae@gmail.com>
2024-03-27 00:40:56 -0700

    correct syntax

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,12 +6,11 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *Temp;
-
 	if (tree != NULL)
 	{
 		binary_tree_delete(tree->left);
 		binary_tree_delete(tree->right);
 		free(tree);
 	}
+	return;
 }
