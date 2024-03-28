0f4710d3a77616aa87816368bcfb4fce886454e5
Rajae <hemmanyrajae@gmail.com>
2024-03-27 18:56:26 -0700

    up"

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,6 +13,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left || tree->right)
-		C = 1 +binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+		C = 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
 	return (C);
 }
